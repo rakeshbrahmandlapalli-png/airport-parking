056750d58d1cdcc077c99e73bd94ccf5202e8f32
Rakesh <you@example.com>
2026-03-27 23:53:15 +0000

    Forced Prisma to use live environment URL

--- app/prismadb.ts
+++ app/prismadb.ts
@@ -1,10 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prismadb = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+const prismadb = globalThis.prisma ?? prismaClientSingleton();
 
 export default prismadb;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
