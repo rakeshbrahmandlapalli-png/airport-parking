2fbaccf737192d5d3980d4dbc416e25ad51c900b
Rakesh <you@example.com>
2026-03-28 00:55:20 +0000

    Added dummy URL to bypass Vercel build crash

--- app/prismadb.ts
+++ app/prismadb.ts
@@ -1,12 +1,27 @@
 import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  // If Vercel hides the real URL during build, use a fake one so it doesn't crash!
+  const connectionString = 
+    process.env.DATABASE_URL || 
+    process.env.DATABASE_URL_NEW || 
+    "postgresql://dummy:password@localhost:5432/dummy?sslmode=require";
+
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: connectionString,
+      },
+    },
+  });
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-// Let Prisma find the URL automatically from the schema
-const prismadb = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+const prismadb = globalThis.prisma ?? prismaClientSingleton();
 
 export default prismadb;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
